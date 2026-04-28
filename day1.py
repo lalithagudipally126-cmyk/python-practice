36a50e41e269f8c4b95e9f38f4948060975427dd
lalithagudipally126-cmyk <lalithagudipally126@gmail.com>
2026-04-28 20:22:14 +0530

    day1 practice of python

---
+++ day1.py
@@ -0,0 +1,24 @@
+#initialize the variable
+a=100
+#printing the variable
+print(a)
+print(a,"200")
+#doing a lil math
+age=20
+next_year_age= age+1
+previous_year_age= age-1
+double_age= age*2
+half_the_age= age/2
+print("age:", age, "next year age:", next_year_age,"previous year age", previous_year_age, "double the age", double_age, "half the age", half_the_age)
+#working with strings
+first_name = "lalitha"
+last_name = "gudipalli"
+full_name = first_name + " " + last_name
+print("full name:", full_name)
+# + is used to join the strings 
+# " " is used to create space between the two strings
+print(f"my name is {full_name} my age is {age}")
+# the f before the quotes lets you insert variables directly inside {}
+# giving input to the variables
+user_name = input("Enter your name: ")
+print("Hello,", user_name)
