e04dad92416ab6a5644a0314b01ecdd7730c4d96
lalithagudipally126-cmyk <lalithagudipally126@gmail.com>
2026-05-18 15:55:49 +0530

    sqaure of numbers 10-1, multiplication table

---
+++ day12.py
@@ -0,0 +1,13 @@
+#printing sqaure of numbers from 10 to 1
+n=10
+for i in range(n):
+    s=n*n
+    print(n,"*",n,":",s)
+    n-=1
+#multiplication of table using for loop
+m=int(input("enter number:"))
+j=1
+for j in range(11):
+    t=m*j
+    print(f"{m}*{j}={t}")
+    
