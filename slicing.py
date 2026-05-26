4ca91c1fb8a417e96ea16f009924aa01d5bb34a4
lalithagudipally126-cmyk <lalithagudipally126@gmail.com>
2026-05-26 12:45:53 +0530

    slicing

---
+++ slicing.py
@@ -0,0 +1,58 @@
+#Q1.  Take the list [2,4,6,8,10,12,14] and slice out [6,8,10].
+
+list1 =[2,4,6,8,10,12,14]
+print(list1[2:5])
+
+#Q2.  From the string "DEVELOPER", slice to get "LOPE".
+str1 = "DEVELOPER"
+print(str1[4:8])
+
+#Q3.  Given [1,3,5,7,9,11,13,15], slice to get [15,11,7,3].
+list2 = [1,3,5,7,9,11,13,15]
+print(list2[::-2])
+
+#Q4.  From "PYTHONIC", slice to get "NIC".
+str2 = "PYTHONIC"
+print(str2[5::])
+
+#Q5. (Challenge)  Take [100,200,300,400,500,600,700] and slice to get [700,500,300,100].
+list3 = [100,200,300,400,500,600,700]
+print(list3[::-2])
+
+#Q6.  Take the list [5,10,15,20,25,30,35] and slice out [10,15,20].
+list4 = [5,10,15,20,25,30,35]
+print(list4[1:4])
+
+#Q7. From the string "PYTHONIC", slice to get "HON".
+print(str2[3:6])
+
+#Q8.  Given [1,2,3,4,5,6,7,8,9], slice to get [9,7,5,3].
+list5 = [1,2,3,4,5,6,7,8,9]
+print(list5[:1:-2])
+
+#Q9.  From "DEVELOPER", slice to get "VELO".
+print(str1[2:6])
+
+#Q10. (Challenge)  Take [100,200,300,400,500,600] and slice to get [600,400,200].
+list6 = [100,200,300,400,500,600]
+print(list6[::-2])
+
+#Q11.  Take the list `[2,4,6,8,10,12,14,16]` and slice out `[12,10,8]`.
+list7 = [2,4,6,8,10,12,14,16]
+print(list7[5:2:-1])
+
+#Q12.  From the string `"PROGRAMMING"`, slice to get `"GRAM"`.
+str3 = "PROGRAMMING"
+print(str3[3:7])
+
+#Q13.  Given `[1,2,3,4,5,6,7,8,9,10]`, slice to get `[8,6,4,2]`.
+list8 = [1,2,3,4,5,6,7,8,9,10]
+print(list8[7::-2])
+
+#Q14. From `"DEVELOPER"`, slice to get `"ELO"` but **in reverse order**.
+print(str1[3:6])
+
+#Q15. (Boss Challenge)  Take `[100,200,300,400,500,600,700,800]` and slice to get `[700,400,100]`.
+list9 = [100,200,300,400,500,600,700,800]
+print(list9[6::-3])
+
